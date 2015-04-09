032223f10dd96d7fc584df4bc403082b31b5fe72
Ben Johnson <ben@bjmfactory.com>
2015-04-09 11:58:01 -0500

    changes bing_id to google_id; the end of an era

--- schema.sql
+++ schema.sql
@@ -3,18 +3,18 @@ CREATE DATABASE nomNow;
 USE nomNow;
 
 CREATE TABLE restaurants (
-  bing_id varchar NOT NULL,
+  google_id varchar NOT NULL,
   name varchar NOT NULL,
   longitude int NOT NULL,
   latitude int NOT NULL,
-  PRIMARY KEY(bing_id)
+  PRIMARY KEY(google_id)
 );
 
 CREATE TABLE reports (
   id int NOT NULL auto_increment,
-  bing_id varchar NOT NULL,
+  google_id varchar NOT NULL,
   wait_time int NOT NULL,
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY(id),
-  FOREIGN KEY(bing_id) REFERENCES restaurants(bing_id)
+  FOREIGN KEY(google_id) REFERENCES restaurants(google_id)
 );

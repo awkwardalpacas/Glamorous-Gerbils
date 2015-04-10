25e6335cfb172ae8aba573034f0575d82af1d127
Ben Johnson <ben@bjmfactory.com>
2015-04-10 12:32:00 -0500

    removes illegal newlines from temporary server responses

--- server/app.js
+++ server/app.js
@@ -2,14 +2,11 @@ var express = require('express');
 var app = express();
 
 app.get('/wait', function (req, res) {
-  res.send('received a GET request at /wait. This will eventually ask the database
-    for the wait times based for the restaurants in that area and then send them back
-    to Angular.');
+  res.send('received a GET request at /wait. This will eventually ask the database for the wait times based for the restaurants in that area and then send them back to Angular.');
 });
 
 app.post('/wait', function (req, res) {
-  res.send('received a POST request at /wait. This will eventually accept a wait time
-    from Angular and add it to the database.');
+  res.send('received a POST request at /wait. This will eventually accept a wait time from Angular and add it to the database.');
 });
 
 app.use('/', express.static('client'));

ce16d914406509e6195fa37f69755dca94c55c02
Phil Dornfeld <phillydorn75@gmail.com>
2015-04-10 20:07:04 -0500

    Changing markers to images

--- client/app/services.js
+++ client/app/services.js
@@ -29,8 +29,6 @@ angular.module('nomNow.services', [])
       });
   }
 
-
-
   var getPosition = function () {
     var deferred = $q.defer();
     navigator.geolocation.getCurrentPosition(function(position){
@@ -52,20 +50,35 @@ angular.module('nomNow.services', [])
       console.log('inside', resp.data)
       restaurants = resp.data;
       for (var i = 0; i<resp.data.length; i++) {
-        getRestaurantLocation(resp.data[i].google_id);
+        getRestaurantLocation(resp.data[i].google_id, resp.data[i].wait);
       }
       return resp.data;
     })
   }
 
-  var getRestaurantLocation = function(id) {
+  var getRestaurantLocation = function(id, wait) {
     var request = {placeId : id};
     var service = new google.maps.places.PlacesService(map);
+    var wait = wait-(wait%5);
     service.getDetails(request, function (place, status) {
       if (status === google.maps.places.PlacesServiceStatus.OK) {
         var coords = new google.maps.LatLng(place.geometry.location.k, place.geometry.location.D)
-        var name = place.name
-        createMarker(map, coords, name);
+        var image = {
+          url : './images/'+wait+'.jpg',
+          size: new google.maps.Size(50,50),
+          origin: new google.maps.Point(0,0),
+          anchor: new google.maps.Point(25,50)
+        }
+        console.log(image.url)
+
+
+
+        var marker = new google.maps.Marker({
+            position: coords,
+            map: map,
+            icon: image
+        });
+        // createMarker(map, coords, name);
       }
     })
   }

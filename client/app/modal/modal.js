90ae6160affde757a671614995bb4479255243ec
vishalatmakuri <findvishal@gmail.com>
2015-04-16 16:19:52 -0500

    code clean up

--- client/app/modal/modal.js
+++ client/app/modal/modal.js
@@ -7,39 +7,39 @@ nomNow.controller("modalctrl", ["$scope","$modal","Map",function ($scope,$modal,
       size: 'lr',
       
   });
-    //the angular docs required this step. the popup does not work without this.
+    //the angular docs required this step. the popup does not work without this
     modalInstance.result.then(function(selectedItem){
       $scope.selected= selectedItem;
       },function(){
         $log.info('Modal dismissed at: ' + new Date());
     }) 
-  };
-}]);
+  }
+}])
 
 .controller('ModalInstanceCtrl',["$scope","$modalInstance",'Map',"$http",function ($scope, $modalInstance, Map, $http){
-  $scope.manual = {'visibility' :"collapse"}
-  $scope.buttons = {'visibility' :"collapse"}
-  // loading exists till it finds the closest.
-  $scope.items="Loading..."
+  //this makes the options invisible till they are needed.
+  $scope.manual = {'visibility' :"collapse","height":"0px","width":"0px"};
+  $scope.buttons = {'visibility' :"collapse"};
+  // loading exists till it finds the closest result 
+  $scope.items="Loading...";
   $scope.waittime={};
   var restaurant;
-  //  changes the text on init to ask if you are at.
+//get close lets us change the text at the start.
   var getclose = function(){
     Map.getClosestRestaurant(function(value){
-      console.log(value)
       restaurant=value;
-
       $scope.items = "Are you  at : "+value['name']+"?";
-      $scope.buttons = {'visibility' :"visible"}
-      $scope.$digest()
+      $scope.buttons = {'visibility' :"visible"};
+      $scope.$digest();
     }) 
-  }
+  };
 
-  getclose()
-
-  $scope.waittime.inputs ='30'
+  getclose();
+  // this lets 30 be defalut text till input changes
+  $scope.waittime.inputs ='30';
+  // below are the button functions for the modal
   $scope.yes=function(){
-    $scope.items= restaurant['name']
+    $scope.items= restaurant['name'];
     $scope.buttons = {'visibility' :"collapse", "height":"0px","width":"0px"};
   }
 
@@ -51,41 +51,39 @@ nomNow.controller("modalctrl", ["$scope","$modal","Map",function ($scope,$modal,
 
   $scope.cancel=function(){
     $modalInstance.close();
-  };
+  }
 
   $scope.ok =function(){
    var n = $scope.manualinput;
     Map.getClosestRestaurant(function(value){
       if(value['name']){
-        restaurant=value;
-        $scope.items = "Are you  at : "+value['name']+"?";
-        $scope.buttons = {'visibility' :"visible"};
-        $scope.$digest();
+      restaurant=value;
+      $scope.items = "Are you  at : "+value['name']+"?";
+      $scope.buttons = {'visibility' :"visible"};
+      $scope.$digest();
       }
       else{
-        $scope.items = "sorry no location found";
-        $scope.manual = {'visibility' :"visible"};
-        $scope.$digest();
+         $scope.items = "sorry no location found";
+          $scope.manual = {'visibility' :"visible"};
+          $scope.$digest();
       }
     },n);      
     $scope.manual = {'visibility' :"collapse"};
-  };
-
+  }
+  //on submit we have a success to get new wait times form the server
   $scope.submit= function(){
-    //info goes
     var info= {data:{google_id:restaurant.google_id,
       name:restaurant.name,
       longitude: restaurant.location["D"],
       latitude: restaurant.location["k"],
       'wait':$scope.waittime.inputs}};
-
     $http({
       method: 'POST',
       url: '/wait',
       data: info
     }).success(function(){
       Map.findWaitTimes();
-    })
+    });
     $modalInstance.close();
   };
 
